741c4e82cb621129a3a3d6b5260c9da26e7ca1df
Jalal KADDOURI <jalalkadori2@gmail.com>
2022-11-27 04:59:45 +0000

    adding script

--- script.js
+++ script.js
@@ -3,23 +3,56 @@ function Ajouter() {
 
     // User Input 
     let Nom = document.getElementById('nom').value;
-    let Marque = document.getElementById('marque');
-    let Prix = document.getElementById('prix');
-    let Date = document.getElementById('date');
-    let Type = document.getElementById('type');
+    let Marque = document.getElementById('marque').value;
+    let Prix = document.getElementById('prix').value;
+    let Date = document.getElementById('date').value;
+    let Type = document.getElementById('type').value;
     let noPromo = document.getElementById('noPromo');
     let Promo = document.getElementById('Promo');
 
     //creating new table Row 
-    var table = document.querySelector('table');
-    var tr = document.createElement('tr');
-    var td = document.createElement('td');
+    let table = document.querySelector('table');
+    let tBody = document.querySelector('tbody');
 
-    table.append(tr);
-    tr.appendChild(td);
+    for (let i = 0; i < 1; i++) {
+        const row = document.createElement("tr")
+        const noPromo = document.querySelector('noPromo');
+        const Promo = document.querySelector('Promo');
+            for (let j = 0; j < 7; j++) {
+                const cell = document.createElement("td");
+                row.appendChild(cell);
+                if(j == 0){
+                    cell.innerText = Nom;
+                }
+                if(j == 1){
+                    cell.innerText = Marque;
+                }
+                if(j == 2){
+                    cell.innerText = Prix;
+                }
+                if(j == 3){
+                    cell.innerText = Type;
+                }
+                if(j == 4){
+                    cell.innerText = Date;
+                }
+                if(j == 5){
+                    cell.innerText = Non;
+                }
 
-    td.innerText = Nom;
+                if(j == 6){
+                    cell.innerText = Nom;
+                }
+            }
+        table.appendChild(row);
+    }
+    
+    
+
+    
+  
    
+    
     
 
 
